0ff642cdee5be14da12e994a7e95b7717633403f
Sheetal Mangate <sheetal.mangate@gmail.com>
2024-12-12 12:17:45 -0800

    code fixes for render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,44 +1,48 @@
-import express from 'express';
-import path from 'node:path';
-import type { Request, Response } from 'express';
-import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './utils/auth.js';
-
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { authenticateToken } from "./utils/auth.js";
+import { typeDefs, resolvers } from "./schemas/index.js";
+import db from "./config/connection.js";
 import { fileURLToPath } from "node:url";
+import path from "node:path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const startApolloServer = async () => {
   await server.start();
   await db;
-
-  const PORT = process.env.PORT || 3001;
-  const app = express();
-
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }
-  ));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(
+    "/graphql",
+    expressMiddleware(server as any, {
+      context: authenticateToken as any,
+    }),
+  );
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  // if we're in production, serve client/dist as static assets so everything works off one port,
+  // no more dev server on client.
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+    // necessary for client-side routing to work
+    app.get("*", (_req, res) => {
+      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
     });
   }
+
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
